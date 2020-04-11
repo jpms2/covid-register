11a9c71675cb94eed463b4cf1a7f396ca8414616
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 13:56:15 -0300

    [fix] Give correct code on error

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -9,20 +9,20 @@ class UserSerializer {
 
     create(user) {
         console.log(`Creating new user: ${user.username}`)
+        var httpCode = 201
 
         this.client.query(`INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`, function(error, rows){
             if (error) {
                 console.log("Received error from mysql")
                 if (error.code == 'ER_DUP_ENTRY' || error.errno == 1062) {
-                    return 409
+                    httpCode = 409
                 } else {
-                    return 500
+                    httpCode = 500
                 }
             }
         })
 
-        console.log("Query succeeded")
-        return 201
+        return httpCode
     }
 }
 
