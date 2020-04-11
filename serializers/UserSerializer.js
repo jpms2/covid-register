a850ccae6c0818b61e3ad568ca16bf143cf7083f
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 18:07:52 -0300

    [add] logs

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -10,10 +10,10 @@ class UserSerializer {
     create(user) {
         var httpCode = 201
         var queryValue = `INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`
-        this.client.query(queryValue).then(result => {
+        var result = this.client.query(queryValue)
             console.log("Worked! " + JSON.stringify(result))
             return httpCode
-        }).catch(err => {
+
             console.log("Error: "+ JSON.stringify(err))
             if (err.code && err.errno) {
                 if (err.code == 'ER_DUP_ENTRY' || err.errno == 1062) {
@@ -24,7 +24,6 @@ class UserSerializer {
             }
 
             return httpCode
-        })
     }
 }
 
