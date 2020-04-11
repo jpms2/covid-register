51d982e334c42cc1ecc4e48205cbf7cc6443f03c
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 16:19:34 -0300

    [add] Better logs for understading whats going on

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -12,7 +12,10 @@ class UserSerializer {
         var httpCode = 201
         var queryValue = `INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`
         return this.client.query(queryValue)
-          .then(rows => { return httpCode})
+          .then(rows => { 
+            console.log(`request passed with: ${httpCode}`)  
+            return httpCode
+        })
           .catch(err => {
             if (result.code && result.errno) {
                 if (result.code == 'ER_DUP_ENTRY' || result.errno == 1062) {
@@ -22,6 +25,7 @@ class UserSerializer {
                 }
             }
 
+            console.log(`Failed request with: ${httpCode}`)
             return httpCode
         })
     }
