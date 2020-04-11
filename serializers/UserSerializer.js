d4e2d1a6052b622e672f81b428e63ad29d43fc08
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 14:15:17 -0300

    [fix] Result should be a variable

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -11,7 +11,7 @@ class UserSerializer {
         console.log(`Creating new user: ${user.username}`)
         var httpCode = 201
 
-       result = this.client.query(`INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`)
+       var result = this.client.query(`INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`)
        if (result instanceof Error) {
            console.log("Received error from mysql")
            if (error.code == 'ER_DUP_ENTRY' || error.errno == 1062) {
@@ -20,7 +20,7 @@ class UserSerializer {
                httpCode = 500
            }
         }
-        
+
         return httpCode
     }
 }
