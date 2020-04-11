978ecdc96c3faa062214ffe668f7eef0f9eff6a9
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 16:15:55 -0300

    [fix] Should return the promise value

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -11,9 +11,9 @@ class UserSerializer {
         console.log(`Creating new user: ${user.username}`)
         var httpCode = 201
         var queryValue = `INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`
-        var result = this.client.query(queryValue).then(rows => {
-            return httpCode
-        }).catch(err => {
+        return this.client.query(queryValue)
+          .then(rows => { return httpCode})
+          .catch(err => {
             if (result.code && result.errno) {
                 if (result.code == 'ER_DUP_ENTRY' || result.errno == 1062) {
                     httpCode = 409
