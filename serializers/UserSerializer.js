d8ad3d67cb0bfda00e61f3434d7da8d2c71947ff
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 17:33:37 -0300

    [change] Back to callbacks

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -13,6 +13,7 @@ class UserSerializer {
         var queryValue = `INSERT INTO users (username, password) VALUES ('${user.username}', '${user.password}')`
         return this.client.query(queryValue, function(err, result) {
             if(err) {
+                console.log(JSON.stringify(err))
                 if (result.code && result.errno) {
                     if (result.code == 'ER_DUP_ENTRY' || result.errno == 1062) {
                         httpCode = 409
@@ -21,7 +22,7 @@ class UserSerializer {
                     }
                 }
             }
-
+            console.log(JSON.stringify(httpCode))
             return httpCode
         })
     }
