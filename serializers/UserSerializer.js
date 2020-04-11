7a075e73211e5cd6e92fcbc0b7ffddf88a7f2e72
jpms2 <jpms2@cin.ufpe.br>
2020-04-11 10:27:16 -0300

    [fix] User serializer was not creating a proper query

--- serializers/UserSerializer.js
+++ serializers/UserSerializer.js
@@ -12,7 +12,7 @@ class UserSerializer {
             throw new Error("Failed to create user, could not establish connection to database")
         }
 
-        this.client.query(`INSERT INTO user (username, password) VALUES (${user.username}, ${user.password})` function(error, rows){
+        this.client.query(`INSERT INTO user (username, password) VALUES (${user.username}, ${user.password})`, function(error, rows){
             if (error) {
                 if (error.code == 'ER_DUP_ENTRY' || error.errno == 1062) {
                     throw new UsernameAlreadyRegisteredError("Username is taken")
