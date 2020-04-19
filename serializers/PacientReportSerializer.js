73bc3b5e438bebc63c447c77acf1dd73a12bc316
jpms2 <jpms2@cin.ufpe.br>
2020-04-19 15:06:49 -0300

    [fix] find report by pacient was undefined

--- serializers/PacientReportSerializer.js
+++ serializers/PacientReportSerializer.js
@@ -9,6 +9,13 @@ class PacientReportSerializer {
         const query = `INSERT INTO pacient_report (pacient_ID, report_ID) VALUES ('${cpf}', '${report_ID}')`
         await this.client.query(query);
     }
+
+    async find(cpf) {
+        const query = `SELECT report_ID FROM  pacient_report WHERE cpf = '${cpf}'`
+        const reports = await this.client.query(query)
+
+        return reports
+    }
 }
 
 module.exports = PacientReportSerializer
