d99a27b8065876fa2c10b7108392f1112716cb80
jpms2 <jpms2@cin.ufpe.br>
2020-04-19 15:03:44 -0300

    [fix] malformed query

--- serializers/PacientReportSerializer.js
+++ serializers/PacientReportSerializer.js
@@ -6,7 +6,7 @@ class PacientReportSerializer {
     }
 
     async create(cpf, report_ID) {
-        const query = `INSERT INTO pacient_report (pacient_ID, report_ID) VALUES '${cpf}', '${report_ID}'`
+        const query = `INSERT INTO pacient_report (pacient_ID, report_ID) VALUES ('${cpf}', '${report_ID}')`
         await this.client.query(query);
     }
 }
