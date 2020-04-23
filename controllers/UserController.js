c9cb3a9fffa8673e192a967270d7323aa499058c
jpms2 <jpms2@cin.ufpe.br>
2020-04-23 15:31:59 -0300

    [fix] salt rounds should be an int

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,7 +10,7 @@ class UserController{
     }
 
     async create(user) {
-        const salt = await bcrypt.genSalt(process.env.SALT_ROUNDS)
+        const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS, 10))
         const hash = bcrypt.hash(user.password, salt)
 
          const httpCode = await this.userSerializer.create({username: user.username, password: hash})
